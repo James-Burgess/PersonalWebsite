16aaa290a87a4c2fbc70d05521b54d40fcbfdd42
james-prodigy <james.burgess@prodigyfinance.com>
2020-05-28 15:19:26 +0200

    add transitions to about and work page
    Signed-off-by: james-prodigy <james.burgess@prodigyfinance.com>

---
+++ public/assets/scripts/main.js
@@ -0,0 +1,109 @@
+
+// Refs
+const homeContainer = document.querySelector('#home-container'),
+    aboutBtn = document.querySelector('#about-button'),
+    content = document.querySelector('#content'),
+    workContent = document.querySelector('#work-content'),
+    btnClose = document.querySelector('#close'),
+    workBtn = document.querySelector('#work-button'),
+    aboutSvg = document.querySelector('#about-svg'),
+    workSvg = document.querySelector('#work-svg');
+
+
+var showingWork = false
+var showingAbout = false
+
+// Anime.js Commons Values for SVG Morph
+const common = {
+    targets: '.polymorph',
+    easing: 'easeOutQuad',
+    duration: 600,
+    loop: false };
+
+var showAbout = function () {
+    hideWork();
+    showingWork = false;
+
+    homeContainer.classList.remove('active');
+
+    aboutSvg.classList.add('active');
+    content.classList.add('active');
+
+    anime({
+        ...common,
+        fill: ['#e3e3e3', 'rgba(0,200,200,0.5)'],
+        points: [
+            { value: '215,110 0,110 200,100 215,0' }] });
+}
+
+var showWork = function() {
+    hideAbout();
+    showingAbout = false
+
+    homeContainer.classList.remove('active');
+
+    workSvg.classList.add('active');
+    workContent.classList.add('active');
+
+    anime({
+        targets: '.work-polymorph',
+        easing: 'easeOutQuad',
+        duration: 600,
+        loop: false,
+        fill: ['#e3e3e3', 'rgba(10,100,200,0.5)'],
+        points: [
+            { value: '215,110 0,110 200,100 215,0' }] });
+}
+
+var hideAbout = function() {
+    content.classList.remove('active');
+    homeContainer.classList.add('active');
+
+    anime({
+        ...common,
+        fill: '#e3e3e3',
+        points: [
+            {value: '215,110 0,110 0,0 215,0'}
+        ],
+        complete: function() {
+            aboutSvg.classList.remove('active');
+        }
+    })
+}
+
+var hideWork = function() {
+    homeContainer.classList.add('active');
+    workContent.classList.remove('active');
+
+
+    anime({
+        targets: '.work-polymorph',
+        easing: 'easeOutQuad',
+        duration: 600,
+        loop: false,
+        fill: '#e3e3e3',
+        points: [
+            {value: '215,110 0,110 0,0 215,0'}
+        ],
+        complete: function() {
+            workSvg.classList.remove('active');
+        }
+    })
+}
+
+aboutBtn.addEventListener('click', () => {
+    showingAbout ? hideAbout() : showAbout()
+    showingAbout = !showingAbout
+});
+
+workBtn.addEventListener('click', () => {
+    showingWork ? hideWork() : showWork()
+    showingWork = !showingWork
+});
+
+btnClose.addEventListener('click', () => {
+    hideAbout()
+    showingAbout = false
+});
+
+
