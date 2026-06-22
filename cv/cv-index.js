68e506ac7f6482a7ad78b4491a8187479c34d12a
james-burgess <mail@jimmyb.co.za>
2026-06-22 12:14:22 +0200

    earthcv

--- cv/cv-index.js
+++ cv/cv-index.js
@@ -2,6 +2,7 @@ import conservation_volunteerData from "./data/cv-conservation-volunteer.json";
 import cxl_devData from "./data/cv-cxl-dev.json";
 import defaultData from "./data/cv-default.json";
 import dive_instructorData from "./data/cv-dive-instructor.json";
+import greeneData from "./data/cv-greene.json";
 import wildlifeactData from "./data/cv-wildlifeact.json";
 
-export const allCvs = [conservation_volunteerData, cxl_devData, defaultData, dive_instructorData, wildlifeactData];
+export const allCvs = [conservation_volunteerData, cxl_devData, defaultData, dive_instructorData, greeneData, wildlifeactData];
